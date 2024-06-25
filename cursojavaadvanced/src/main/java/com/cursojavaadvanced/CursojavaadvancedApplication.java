17c11a21066dcc4f04c31dbd06e1c34f069c0d2f
Laura <laura@hotmail.com>
2024-06-25 01:11:55 -0500

    Added Originalk code

---
+++ cursojavaadvanced/src/main/java/com/cursojavaadvanced/CursojavaadvancedApplication.java
@@ -0,0 +1,13 @@
+package com.cursojavaadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavaadvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavaadvancedApplication.class, args);
+	}
+
+}
